910cafe36ac7b3c3ffb5ee13138587402c5ed018
Rachit Singh <rachit.singh2024@gmail.com>
2025-03-17 21:26:22 +0530

    Route Groups

---
+++ route_groups_challenge/src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <h1>Please login to see the content</h1>
+  )
+}
+
+export default Login
---
+++ route_groups_challenge/src/app/(users)/userInfo/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UsersInfo = () => {
+  return (
+    <div>
+      <h1>User Information</h1>
+      <p>Here are the details about the user.</p>
+    </div>
+  );
+};
+
+export default UsersInfo;
